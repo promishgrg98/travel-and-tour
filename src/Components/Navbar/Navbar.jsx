81102c0a46565e448a62371f45a3a8dba03e3b17
Promish Gurung <promishgrg98@gmail.com>
2024-06-04 19:59:44 +0545

    add icons on navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './navbar.css';
 import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 function Navbar() {
     return (
@@ -10,6 +12,40 @@ function Navbar() {
                     <a href='#' className='logo flex'>
                         <h1><MdOutlineTravelExplore className='icon' /> Travel.</h1>
                     </a>
+                </div>
+                <div className='navBar'>
+                    <ul className='navLists flex'>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>Home</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>Packages</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>Shop</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>About</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>Pages</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>News</a>
+                        </li>
+                        <li className='navItem'>
+                            <a href='#' className='navLink'>Contact</a>
+                        </li>
+                        <button className='btn'>
+                            <a href='#'>BOOK NOW</a>
+                        </button>
+                    </ul>
+                    <div className='closeNavbar'>
+                        <AiFillCloseCircle className='icon' />
+                    </div>
+                </div>
+                <div className='toggleNavbar'>
+                    <TbGridDots className='icon' />
                 </div>
             </header>
         </section>
