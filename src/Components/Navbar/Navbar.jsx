167e8e3ff31a738a1c3a54a516dfb654a48054b1
Promish Gurung <promishgrg98@gmail.com>
2024-06-06 12:11:04 +0545

    add toggle feature for navbar in smaller screen

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import { MdOutlineTravelExplore } from "react-icons/md";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { TbGridDots } from "react-icons/tb";
 
 function Navbar() {
+    const [active, setActive] = useState('navBar');
+    const showNav = () => {
+        setActive('navBar activeNavbar');
+    }
+    const removeNavbar = () => {
+        setActive('navBar');
+    }
+
     return (
         <section className='navBarSection'>
             <header className='header flex'>
@@ -13,7 +21,7 @@ function Navbar() {
                         <h1><MdOutlineTravelExplore className='icon' /> Travel.</h1>
                     </a>
                 </div>
-                <div className='navBar'>
+                <div className={active}>
                     <ul className='navLists flex'>
                         <li className='navItem'>
                             <a href='#' className='navLink'>Home</a>
@@ -40,11 +48,11 @@ function Navbar() {
                             <a href='#'>BOOK NOW</a>
                         </button>
                     </ul>
-                    <div className='closeNavbar'>
+                    <div onClick={removeNavbar} className='closeNavbar'>
                         <AiFillCloseCircle className='icon' />
                     </div>
                 </div>
-                <div className='toggleNavbar'>
+                <div onClick={showNav} className='toggleNavbar'>
                     <TbGridDots className='icon' />
                 </div>
             </header>
