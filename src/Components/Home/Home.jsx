737b6b723b072424967c2972d189aa2bf073276a
Promish Gurung <promishgrg98@gmail.com>
2024-06-08 21:32:18 +0545

    add home.css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,13 @@ function Home() {
     <section className='home'>
         <div className='overlay'></div>
         <video src={video1} muted autoPlay loop type='video/mp4'></video>
+
+        <div className='homeContent container'>
+          <div className='textDiv'>
+            <span className='smallText'>Our Packages</span>
+            <h1 className='homeTitle'>Search your Holiday</h1>
+          </div>
+        </div>
     </section>
   )
 }
