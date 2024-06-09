2a50f58b217535de8714b323a22b8d54e33c5a05
Promish Gurung <promishgrg98@gmail.com>
2024-06-09 22:19:52 +0545

    add input for searching destination

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.css';
 import video1 from '../../Assets/video1.mp4';
+import { GrLocation } from "react-icons/gr";
 
 function Home() {
   return (
@@ -12,6 +13,31 @@ function Home() {
           <div className='textDiv'>
             <span className='smallText'>Our Packages</span>
             <h1 className='homeTitle'>Search your Holiday</h1>
+          </div>
+
+          <div className='cardDiv grid'>
+            <div className='destinationInput'>
+              <label htmlFor='city'>Search your Destination</label>
+              <div className='input flex'>
+                <input type='text' placeholder='Enter name here...' />
+                <GrLocation className='icon' />
+              </div>
+            </div>
+            <div className='dateInput'>
+              <label htmlFor='date'>Select your date:</label>
+              <div className='input flex'>
+                <input type='date' />
+              </div>
+            </div>
+            <div className='priceInput'>
+              <div className='label_total flex'>
+                <label htmlFor='price'>Max price:</label>
+                <h3 className='total'>$5000</h3>
+              </div>
+              <div className="input flex">
+                <input type='range' max={5000} min={1000} />
+              </div>
+            </div>
           </div>
         </div>
     </section>
