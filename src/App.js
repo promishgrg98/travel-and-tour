c0ef90e84d5cc52acb49a082bc68af62bb9c84a0
Promish Gurung <promishgrg98@gmail.com>
2024-06-14 22:48:30 +0545

    add video for footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './footer.css';
+import video2 from '../../Assets/video2.mp4';
+import { FiSend } from "react-icons/fi";
+
+function Footer() {
+    return (
+        <section className="footer">
+            <div className="videoDiv">
+                <video src={video2} autoPlay loop muted type='video/mp4'></video>
+            </div>
+
+            <div className="secContent container">
+                <div className="contactDiv flex">
+                    <div className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
+
+                    <div className="inputDiv flex">
+                        <input type='text' placeholder='Enter Email Address' />
+                        <button className='btn flex' type='submit'>
+                            SEND <FiSend className='icon'/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
